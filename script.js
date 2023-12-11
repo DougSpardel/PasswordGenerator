d0210e6903d14e5e92574bceeeb522fe9382e091
DougSpardel <85414095+DougSpardel@users.noreply.github.com>
2023-12-11 15:24:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// Assignment code here
+function generatePassword() {
+  // Prompt the user for the password length
+  var passwordLength = parseInt(
+    prompt("Enter the length of the password (between 8 and 128 characters):")
+  );
+
+  // Validate the password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
+
+  // Prompt the user for character types to include
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumeric &&
+    !includeSpecial
+  ) {
+    alert("At least one character type must be selected.");
+    return "";
+  }
+
+  // Define character sets based on user selections
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  // Combine character sets based on user selections
+  var allChars = "";
+  if (includeLowercase) allChars += lowercaseChars;
+  if (includeUppercase) allChars += uppercaseChars;
+  if (includeNumeric) allChars += numericChars;
+  if (includeSpecial) allChars += specialChars;
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
